eb73ee250195078d7a31abc1071782cb4f5dc7cf
sauravBarua <myallmailofficial@gmail.com>
2022-10-25 04:51:33 +0200

    working on json data

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const programmings = require('./data/programmings.json')
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/programmings", (req, res) => {
+    res.send(programmings);
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});

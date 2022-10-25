ee5f5468022df99abc73511bcb94f09d33a2a20c
sauravBarua <myallmailofficial@gmail.com>
2022-10-25 10:52:27 +0200

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 const programmings = require("./data/programmings.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

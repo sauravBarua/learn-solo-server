6fc756b4d36c80d8525f8c3bced012c3d47428c8
sauravBarua <myallmailofficial@gmail.com>
2022-10-25 05:07:41 +0200

    add params

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const programmings = require('./data/programmings.json')
+const programmings = require("./data/programmings.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/programmings", (req, res) => {
-    res.send(programmings);
-  });
+  res.send(programmings);
+});
+
+app.get("/programmings/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedProgrammings = programmings.find((p) => p._id === id);
+  res.send(selectedProgrammings);
+  //   console.log(req.params.id);
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
